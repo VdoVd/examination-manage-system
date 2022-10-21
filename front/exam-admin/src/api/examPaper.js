27aeb7edb2b7aaa75bb906d7e1665a0d7bf25dba
hello-express <liuhaobin486@gmail.com>
2022-10-21 18:16:48 +0800

    finish front

--- front/exam-admin/src/api/examPaper.js
+++ front/exam-admin/src/api/examPaper.js
@@ -1,10 +1,9 @@
-import {post} from '@/utils/request'
-import {query} from "vue/src/platforms/web/util/index";
+import { post } from '@/utils/request'
 
 export default {
-  pageList: query => post('/api/admin/exam/paper/page',query),
-  taskExamPage: query =>post('/api/admin/exam/paper/taskExamPage',query),
-  edit: query=>post('/api/admin/exam/paper/select/',query),
-  select: id=>post('/api/admin/exam/paper/select/'+id),
-  deletePaper: id=>post('/api/admin/exam/paper/delete'+id)
+  pageList: query => post('/api/admin/exam/paper/page', query),
+  taskExamPage: query => post('/api/admin/exam/paper/taskExamPage', query),
+  edit: query => post('/api/admin/exam/paper/edit', query),
+  select: id => post('/api/admin/exam/paper/select/' + id),
+  deletePaper: id => post('/api/admin/exam/paper/delete/' + id)
 }
--- front/exam-admin/src/api/login.js
+++ front/exam-admin/src/api/login.js
@@ -1,6 +1,6 @@
-import {post,postWithLoadTip} from '@/utils/request'
+import { post, postWithLoadTip } from '@/utils/request'
 
 export default {
-  login:query =>postWithLoadTip(`/api/user/login`,query),
-  logout:query=>post(`/api/user/logout`,query)
+  login: query => postWithLoadTip(`/api/user/login`, query),
+  logout: query => post(`/api/user/logout`, query)
 }
